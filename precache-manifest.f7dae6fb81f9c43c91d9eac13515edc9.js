a9fb5971274fda0d7d54c8f67044b81754da40bb
Gabriel Belmonte <gabelmo92@hotmail.com>
2019-12-17 13:19:46 +0100

    Updates

---
+++ precache-manifest.f7dae6fb81f9c43c91d9eac13515edc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4873729b4a9f9f9c25a0c814a3e296a8",
+    "url": "/pages/index.html"
+  },
+  {
+    "revision": "87994f28847a3f569cd8",
+    "url": "/pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ad2cd8afd88e74c0b2a3",
+    "url": "/pages/static/js/2.5887832e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pages/static/js/2.5887832e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "87994f28847a3f569cd8",
+    "url": "/pages/static/js/main.62428dbe.chunk.js"
+  },
+  {
+    "revision": "f0690d951534ec959e66",
+    "url": "/pages/static/js/runtime-main.602fde14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
